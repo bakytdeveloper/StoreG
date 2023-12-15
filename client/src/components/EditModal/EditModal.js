c8cbd14076a786e299c85699935ce2b42416400f
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-15 17:55:50 +0600

    Создал компонент EditModal, для изменения карточки

---
+++ client/src/components/EditModal/EditModal.js
@@ -0,0 +1,75 @@
+
+// client/src/components/EditModal/EditModal.js
+import React, { useState } from 'react';
+import './EditModal.css';
+
+const EditModal = ({ isOpen, onClose, products, onDeleteClick, onCardClick }) => {
+    const [confirmDelete, setConfirmDelete] = useState(null);
+
+    const handleDeleteClick = (productId) => {
+        setConfirmDelete(productId);
+    };
+
+    const handleConfirmDelete = () => {
+        onDeleteClick(confirmDelete);
+        setConfirmDelete(null);
+        onClose();
+    };
+
+    const handleCancelDelete = () => {
+        setConfirmDelete(null);
+        onClose();
+    };
+
+    return (
+        <>
+            {isOpen && (
+                <div className="edit-modal-overlay">
+                    <div className="edit-modal">
+                        <div className="edit-modal-header">
+                            <h2>Редактирование карточек</h2>
+                            <button onClick={onClose}>&times;</button>
+                        </div>
+                        <div className="edit-modal-content">
+                            <ul>
+                                {products &&
+                                products.map((product) => (
+                                    <li key={product._id}>
+                                            <span onClick={() => onCardClick(product)}>
+                                                {product.name}
+                                            </span>
+                                        <button onClick={() => handleDeleteClick(product._id)}>
+                                            Удалить
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        {/* Добавим окно подтверждения удаления */}
+                        <hr/>
+                        {confirmDelete && (
+                            <div className="delete-confirmation">
+                                <p>
+                                    Вы уверены, что хотите удалить
+                                    <span
+                                        style={{color: "red", fontStyle: "oblique"}}>
+                                        "{products.find(p => p._id === confirmDelete)?.name}"</span>?</p>
+                                {/*Вы уверены, что хотите удалить "{products.find(p => p._id === confirmDelete)?.name}"?</p>*/}
+                                <button onClick={handleConfirmDelete}>Да</button>
+                                <button onClick={handleCancelDelete}>Отмена</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default EditModal;
+
+
+
+
+
+
