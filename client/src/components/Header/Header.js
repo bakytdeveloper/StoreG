28bd718fe1668b1c68285f7fee43b77231fe40c7
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-15 17:59:00 +0600

    Создал компонент Header, заголовок

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,54 @@
+
+// src/components/Header/Header.js
+import React, { useState } from 'react';
+import { FaInstagram, FaWhatsapp, FaTiktok, FaTelegram } from 'react-icons/fa';
+import { FiSearch } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import './Header.css';
+
+const Header = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const history = useHistory();
+
+    const handleSearchChange = (e) => {
+        const value = e.target.value;
+        setSearchTerm(value);
+        onSearch(value);
+    };
+
+    const handleTitleClick = () => {
+        // Очищаем поле поиска и перезагружаем страницу при клике на заголовок
+        setSearchTerm('');
+        history.push('/');
+        window.location.reload(); // Это перезагрузит страницу
+    };
+
+    return (
+        <header className="header">
+            <div className="left-section">
+                <Link to="/" className="site-name" onClick={handleTitleClick}>
+                    <h1 className="site-name"> Store <span>N</span></h1>
+                </Link>
+            </div>
+            <div className="social-icons">
+                <FaInstagram className="icon" onClick={() => window.open('https://www.instagram.com/')} />
+                <FaWhatsapp className="icon" onClick={() => window.open('https://web.whatsapp.com/')} />
+                <a className="phone-number" href="tel:+996312517582">XXX-XX-XX-XX</a>
+                <FaTiktok className="icon" onClick={() => window.open('https://www.tiktok.com/')} />
+                <FaTelegram className="icon" onClick={() => window.open('https://web.telegram.org/')} />
+            </div>
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Поиск..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                <FiSearch className="search-icon" />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
+
