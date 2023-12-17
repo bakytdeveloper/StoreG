942e45233cafe5918e2f1b38623c11cce94d9ef0
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-17 20:11:29 +0600

    Создал локальное хранилище для картинок, при сервере

--- client/src/components/AdminPanel/AddProductForm.js
+++ client/src/components/AdminPanel/AddProductForm.js
@@ -1,100 +1,100 @@
 
-
-import React, { useState } from 'react';
-import './AddProductForm.css';
-
-const AddProductForm = ({ onSubmit }) => {
-    const [formData, setFormData] = useState({
-        type: '',
-        name: '',
-        description: '',
-        price: '',
-        images: [], // Теперь это массив для хранения нескольких изображений
-        specifications: {},
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleSpecificationChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            specifications: {
-                ...prevData.specifications,
-                [name]: value,
-            },
-        }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(formData);
-    };
-
-    return (
-        <form className="add-product-form" onSubmit={handleSubmit}>
-            <label>
-                Type:
-                <input type="text" name="type" value={formData.type} onChange={handleChange} required />
-            </label>
-            <label>
-                Name:
-                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-            </label>
-            <label>
-                Description:
-                <textarea name="description" value={formData.description} onChange={handleChange} required />
-            </label>
-            <label>
-                Price:
-                <input type="number" name="price" value={formData.price} onChange={handleChange} required />
-            </label>
-            <label>
-                Images URL (comma-separated):
-                <input type="text" name="images" value={formData.images.join(',')} onChange={handleChange} required />
-            </label>
-            <label>
-                Specifications:
-                <div className="specifications">
-                    <label>
-                        Color:
-                        <input
-                            type="text"
-                            name="color"
-                            value={formData.specifications.color || ''}
-                            onChange={handleSpecificationChange}
-                        />
-                    </label>
-                    <label>
-                        Screen:
-                        <input
-                            type="text"
-                            name="screen"
-                            value={formData.specifications.screen || ''}
-                            onChange={handleSpecificationChange}
-                        />
-                    </label>
-                    <label>
-                        Screen Size:
-                        <input
-                            type="text"
-                            name="screenSize"
-                            value={formData.specifications.screenSize || ''}
-                            onChange={handleSpecificationChange}
-                        />
-                    </label>
-                    {/* Добавьте другие характеристики по необходимости */}
-                </div>
-            </label>
-            <button type="submit">Add Product</button>
-        </form>
-    );
-};
-
-export default AddProductForm;
+//
+// import React, { useState } from 'react';
+// import './AddProductForm.css';
+//
+// const AddProductForm = ({ onSubmit }) => {
+//     const [formData, setFormData] = useState({
+//         type: '',
+//         name: '',
+//         description: '',
+//         price: '',
+//         images: [],
+//         specifications: {},
+//     });
+//
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             [name]: value,
+//         }));
+//     };
+//
+//     const handleSpecificationChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             specifications: {
+//                 ...prevData.specifications,
+//                 [name]: value,
+//             },
+//         }));
+//     };
+//
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         onSubmit(formData);
+//     };
+//
+//     return (
+//         <form className="add-product-form" onSubmit={handleSubmit}>
+//             <label>
+//                 Type:
+//                 <input type="text" name="type" value={formData.type} onChange={handleChange} required />
+//             </label>
+//             <label>
+//                 Name:
+//                 <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+//             </label>
+//             <label>
+//                 Description:
+//                 <textarea name="description" value={formData.description} onChange={handleChange} required />
+//             </label>
+//             <label>
+//                 Price:
+//                 <input type="number" name="price" value={formData.price} onChange={handleChange} required />
+//             </label>
+//             <label>
+//                 Images URL (comma-separated):
+//                 <input type="text" name="images" value={formData.images.join(',')} onChange={handleChange} required />
+//             </label>
+//             <label>
+//                 Specifications:
+//                 <div className="specifications">
+//                     <label>
+//                         Color:
+//                         <input
+//                             type="text"
+//                             name="color"
+//                             value={formData.specifications.color || ''}
+//                             onChange={handleSpecificationChange}
+//                         />
+//                     </label>
+//                     <label>
+//                         Screen:
+//                         <input
+//                             type="text"
+//                             name="screen"
+//                             value={formData.specifications.screen || ''}
+//                             onChange={handleSpecificationChange}
+//                         />
+//                     </label>
+//                     <label>
+//                         Screen Size:
+//                         <input
+//                             type="text"
+//                             name="screenSize"
+//                             value={formData.specifications.screenSize || ''}
+//                             onChange={handleSpecificationChange}
+//                         />
+//                     </label>
+//
+//                 </div>
+//             </label>
+//             <button type="submit">Add Product</button>
+//         </form>
+//     );
+// };
+//
+// export default AddProductForm;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 // server/index.js
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const multer = require('multer'); // Подключаем multer для обработки файлов
+
 
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -9,6 +13,26 @@ const PORT = process.env.PORT || 5500;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+// Настроим multer для сохранения файлов в папку "uploads"
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+// Разрешим парсинг JSON-запросов
+app.use(bodyParser.json());
+
+// Настроим статический маршрут для раздачи изображений
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // Database connection
 require('./config/db');
