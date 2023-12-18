b239f4fe3843bbec1f35b12a96e7a1d5bdacfc80
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-18 23:41:52 +0600

    Поправил соц. иконки

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,15 @@
 
 // src/components/Header/Header.js
 import React, { useState } from 'react';
-import { FaInstagram, FaWhatsapp, FaTiktok, FaTelegram } from 'react-icons/fa';
+// import { FaInstagram, FaWhatsapp, FaTiktok, FaTelegram } from 'react-icons/fa';
 import { FiSearch } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
+import ins from "./instagram.png"
+import tel from "./telegram.png"
+import what from "./whatsapp.png"
+import tik from "./tik-tok.png"
 import './Header.css';
+// import {hover} from "@testing-library/user-event/dist/hover";
 
 const Header = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -31,11 +36,30 @@ const Header = ({ onSearch }) => {
                 </Link>
             </div>
             <div className="social-icons">
-                <FaInstagram className="icon" onClick={() => window.open('https://www.instagram.com/')} />
-                <FaWhatsapp className="icon" onClick={() => window.open('https://web.whatsapp.com/')} />
+                {/*<FaInstagram className="icon" onClick={() => window.open('https://www.instagram.com/')} />*/}
+
+                <a   href="https://www.instagram.com/" target="_blank">
+                    <img className="icon" src={ins} alt="Instagram Icon" />
+                </a>
+
+                <a href="https://web.whatsapp.com/" target="_blank">
+                    <img className="icon" src={what} alt="Instagram Icon" />
+                </a>
+
+
+                {/*<FaWhatsapp className="icon" onClick={() => window.open('https://web.whatsapp.com/')} />*/}
                 <a className="phone-number" href="tel:+996312517582">XXX-XX-XX-XX</a>
-                <FaTiktok className="icon" onClick={() => window.open('https://www.tiktok.com/')} />
-                <FaTelegram className="icon" onClick={() => window.open('https://web.telegram.org/')} />
+                {/*<FaTiktok className="icon" onClick={() => window.open('https://www.tiktok.com/')} />*/}
+                {/*<FaTelegram className="icon" onClick={() => window.open('https://web.telegram.org/')} />*/}
+
+                <a className="icon"  href="https://www.tiktok.com/" target="_blank">
+                    <img src={tik} alt="Instagram Icon" />
+                </a>
+
+                <a className="icon"  href="https://web.telegram.org/" target="_blank">
+                    <img src={tel} alt="Instagram Icon" />
+                </a>
+
             </div>
             <div className="search-bar">
                 <input
--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -1,26 +1,4 @@
 
-//
-// // client/src/components/ProductCard/ProductCard.js
-// import React from 'react';
-// import './ProductCard.css';
-//
-// const ProductCard = ({ product, onClick }) => {
-//     const { image, name, description, price } = product;
-//
-//     return (
-//         <div className="product-card" onClick={onClick} >
-//             <img src={image} alt={name} className="product-image" />
-//             <div className="product-details">
-//                 <h3 className="product-name">{name}</h3>
-//                 <p className="product-description">{description.slice(0, 70)}</p>
-//                 <p className="product-price">{`Цена: ${price} KGS.`}</p>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default ProductCard;
-
 
 
 import React from 'react';
