dc7fd0922b7fdd1644b8fe379dc1f84e388ff90c
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-21 17:13:42 +0600

    Поправил расстановку в карточках

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -46,9 +46,6 @@ const Sidebar = ({ onTypeSelect }) => {
     return (
         <aside className={`sidebar ${isSidebarVisible ? 'show-sidebar' : ''}`}>
             <h2>Тип товара</h2>
-            {/*<div className="toggle-button" onClick={toggleSidebar}>*/}
-            {/*    ☰*/}
-            {/*</div>*/}
             <ul>
                 <li className={!selectedType ? 'selected' : undefined} onClick={handleShowAllClick}>
                     Все товары
@@ -72,3 +69,34 @@ export default Sidebar;
 
 
 
+// [
+//     {
+//       "direction": "Бытовая техника",
+//       "type": "Телевизор",
+//       "brand": "VIZIO",
+//       "images": ["https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/81pL+uVYD3L._AC_SX679_.jpg", "https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71pSYE0G7OL._AC_SX679_.jpg"],
+//       "name": "Smart TV D",
+//       "description": "40-дюймовый Smart TV VIZIO серии D с разрешением Full HD 1080p, AMD FreeSync, встроенным Apple AirPlay и Chromecast, совместимостью с Alexa, D40f-J09, модель 2022 г..",
+//       "price": 799.99,
+//       "characteristics": [
+//         {"name": "Экрана", "value": "40 дюймов"},
+//         {"name": "Поддерживаемые услуги", "value": "Netflix, Disney+, YouTube, HBO Max"}
+//       ]
+//     },
+//     {
+//       "direction": "Одежда",
+//       "type": "Ботинки",
+//       "brand": "Nike",
+//       "images": ["https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61c0o8n1LjL._AC_SY695_.jpg",
+//        "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61tD2mTthiL._AC_SY695_.jpg",
+//        "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/712blF+gvAL._AC_SY695_.jpg"],
+//       "name": "City",
+//       "description": "Женские классические ботинки Nike City Wheat/Sail, черные (DQ5601 710).",
+//       "price": 129.99,
+//       "characteristics": [
+//         {"name": "Цвет", "value": "Черный"},
+//         {"name": "Материал подошвы", "value": "Резина"},
+//         {"name": "Высота вала", "value": "Ремешок на щиколотке"}
+//       ]
+//     },
+//   ]
